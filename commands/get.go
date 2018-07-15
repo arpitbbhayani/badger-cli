6935825d05c1f4b8c307fd2362a22a45bcce0ea3
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2018-07-15 19:53:19 +0530

    Keys File for get

--- commands/get.go
+++ commands/get.go
@@ -1,16 +1,21 @@
 package commands
 
 import (
-	"errors"
+	"bufio"
+	"os"
 
 	"github.com/arpitbbhayani/badger-cli/sessions"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
+var keysFile string
+
 func init() {
 	getCommand.Flags().StringVarP(&databaseDir, "dir", "", "", "Path to Database Directory (required)")
 	getCommand.MarkFlagRequired("dir")
+
+	getCommand.Flags().StringVarP(&keysFile, "keys-file", "", "", "Path to Keys File")
 
 	BadgerCommand.AddCommand(getCommand)
 }
@@ -18,19 +23,29 @@ func init() {
 var getCommand = &cobra.Command{
 	Use:   "get",
 	Short: "Gets a key",
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return errors.New("requires at least one key")
-		}
-		return nil
-	},
 	Run: func(cmd *cobra.Command, args []string) {
 		sessions.InitBadgerDB("session", databaseDir)
 
 		db := sessions.BadgerDBSessions["session"]
 		txn := db.NewTransaction(false)
 
-		for _, key := range args {
+		keys := args
+
+		if keysFile != "" {
+			fp, err := os.Open(keysFile)
+			if err != nil {
+				logrus.Errorf("error opening file %q: %v", keysFile, err)
+			}
+			defer fp.Close()
+			scanner := bufio.NewScanner(fp)
+			scanner.Split(bufio.ScanLines)
+
+			for scanner.Scan() {
+				keys = append(keys, scanner.Text())
+			}
+		}
+
+		for _, key := range keys {
 			item, err := txn.Get([]byte(key))
 			if err != nil {
 				logrus.Errorf("error while fetching key: '%s', error: %q", key, err)
